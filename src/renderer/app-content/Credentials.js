064912619023fd5bc67f4f8298cb1d0661b87151
Justus Jones <justus1274@gmail.com>
2024-10-11 01:04:30 -0700

    Credentials react component created

---
+++ src/renderer/app-content/Credentials.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function Credentials() {
+  const [user, setUser] = useState("");
+  const [pass, setPassword] = useState("");
+
+  const handleSave = () => {
+    window.electron.saveData({ user, pass }).then((response) => {
+      alert(response);
+    });
+  };
+
+  return (
+    <div className="login-container">
+      <h1>ingradient.</h1>
+      <div className="form-container">
+        <input
+          className="input"
+          type="text"
+          placeholder="Username"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <input
+          className="input password"
+          type="text"
+          placeholder="Password"
+          value={pass}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleSave}>Enter</button>
+        <p className="warning">
+          <em>
+            <b>Warning:</b>
+          </em>{" "}
+          Do not enter any personal information.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Credentials;
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Updated import for React 18
-import App from "./App";
+import Credentials from "./app-content/Credentials";
 
 // Use createRoot for React 18
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<Credentials />);
